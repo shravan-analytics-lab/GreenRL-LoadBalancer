232857d81cc21415550184b352e2b6400d18e6cb
Shravan-Analytics-Lab <shravan.analytics9972@gmail.com>
2026-06-09 19:50:11 +0530

    Delete green_load_balancer directory

--- green_load_balancer/results/plots.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_results(traffic, latency_rl, latency_rule):
-    plt.plot(traffic, label="Traffic")
-    plt.plot(latency_rl, label="RL Latency")
-    plt.plot(latency_rule, label="Rule Latency")
-    plt.legend()
-    plt.title("RL vs Rule-Based")
-    plt.show()
