7559bf1d86c92828f0f944017179a08f928b4a28
Shravan-Analytics-Lab <shravan.analytics9972@gmail.com>
2026-06-09 19:47:05 +0530

    Delete green_load_balancer/baseline directory

--- green_load_balancer/baseline/rule_based.py
+++
@@ -1,6 +0,0 @@
-def rule_based_decision(cpu, servers):
-    if cpu > 80:
-        return 1  # add server
-    elif cpu < 30 and servers > 1:
-        return 2  # remove server
-    return 0  # do nothing
